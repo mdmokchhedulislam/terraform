b286fa681db57acd488ae8203c7ba4da55a80b45
mokchhedulislam <mokchheduls46@gmail.com>
2026-03-17 21:54:11 +0600

    add iam for ec2 which add ssm, cloudwatch, secretmanager and create profile

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,49 +1,58 @@
 
 
-module "alb_sg" {
-  source         = "../../module/security"
-  sg_name        = "my-alb-security-group"
-  sg_description = "Allow HTTP/HTTPS for ALB"
-  vpc_id         = module.vpc.vpc_id
+# module "alb_sg" {
+#   source         = "../../module/security"
+#   sg_name        = "my-alb-security-group"
+#   sg_description = "Allow HTTP/HTTPS for ALB"
+#   vpc_id         = module.vpc.vpc_id
 
-  ingress_rules = [
-    { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
-    { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
-  ]
-  tags = var.tags
-}
+#   ingress_rules = [
+#     { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+#     { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
+#   ]
+#   tags = var.tags
+# }
 
 
-module "my_vpc" {
-  source       = "../../module/vpc"
-  project_name = "dev_vpc"
-  vpc_cidr     = "10.0.0.0/16"
+# module "my_vpc" {
+#   source       = "../../module/vpc"
+#   project_name = "dev_vpc"
+#   vpc_cidr     = "10.0.0.0/16"
 
  
-  public_subnet_map = {
-    "web-1" = { cidr = "10.0.1.0/24", az = "us-east-1a" }
-    "web-2" = { cidr = "10.0.2.0/24", az = "us-east-1b" }
-  }
+#   public_subnet_map = {
+#     "web-1" = { cidr = "10.0.1.0/24", az = "us-east-1a" }
+#     "web-2" = { cidr = "10.0.2.0/24", az = "us-east-1b" }
+#   }
   
-  tags = { Environment = "dev" }
+#   tags = { Environment = "dev" }
+# }
+
+
+# module "alb" {
+#   source = "../../module/ALB"
+
+#   name                = "prod-alb"
+#   internal            = false
+#   security_groups     = module.security.sg_id
+#   subnets             = module.vpc.public_subnet_ids
+#   deletion_protection = true
+#   log_bucket          = aws_s3_bucket.lb_logs.id
+#   log_prefix          = "alb"
+
+#   tags = {
+#     Environment = "production"
+#     ManagedBy   = "terraform"
+#   }
+# }
+
+
+module "iam" {
+  source = "../../module/iam"
+  environment = var.environment
+  project = var.project
+  secrets_arns = var.secrets_arns
+  tags = var.tags
+
+  
 }
-
-
-module "alb" {
-  source = "../../module/ALB"
-
-  name                = "prod-alb"
-  internal            = false
-  security_groups     = module.security.sg_id
-  subnets             = module.vpc.public_subnet_ids
-  deletion_protection = true
-  log_bucket          = aws_s3_bucket.lb_logs.id
-  log_prefix          = "alb"
-
-  tags = {
-    Environment = "production"
-    ManagedBy   = "terraform"
-  }
-}
-
-
